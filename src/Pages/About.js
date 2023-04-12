aaf2114ac7ba22b5af1c0527af386f03c1785a2e
chinapicke <chinapickering@hotmail.co.uk>
2023-04-12 21:52:00 +0800

    finished about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -22,7 +22,7 @@ function About() {
         </div>
         <div className='aboutText'>
           <h1 className='text-5xl font-bold my-8 text-center'>How it all started</h1>
-          <p className='pb-3'>We started from only having 5 cars to over 30 in a matter of years.After more than 5 years in business, we decided to give a fresher look to our brand and our services. With our fully renewed fleet of vehicles, we are ready to meet all expectations and requirements.</p>
+          <p className='pb-3 text-lg text-center mr-3'>We started from only having 5 cars to over 30 in a matter of years. After more than 5 years in business, we decided to give a fresher look to our brand and our services. With our fully renewed fleet of vehicles, we are ready to meet all expectations and requirements.</p>
           <div className='statSection flex justify-between ml-8 pb-10 mt-4 '>
             <div className='ml-8'>
               <span className='statIcon pl-3' ><FontAwesomeIcon icon={faCar} /></span>
@@ -37,13 +37,13 @@ function About() {
               <p className='stat font-semibold'>50+ Mechanics</p>
             </div>
           </div>
-          <h2 className='text-2xl font-semibold mt-12 mb-2 text-center'>Just follow these simple steps</h2>
+          <h2 className='text-2xl font-semibold mt-12 mb-2 text-center italic'>Just follow these simple steps...</h2>
           <div className='aboutFromHomepage grid grid-cols-3 mb-8 mr-3'>
             <div className='onlineSelect '>
               <div className='iconWrapper' id='laptopWrapper'>
                 <FontAwesomeIcon icon={faLaptop} className='aboutIcon w-12 h-10 mb-3' style={{ color: "#000000", }} id='laptopIcon' />
               </div>
-              <h3>Step 1</h3>
+              <h3 className='underline-offset-2'>Step 1</h3>
               <p>Select a car to rent with specific locations and times to pick-up and drop off.</p>
               <p>Recieve an email with 1 hour to confirm your enquiry.</p>
             </div>
@@ -51,8 +51,8 @@ function About() {
               <div className='iconWrapper' id='representativeWrapper'>
                 <img className='aboutIcon w-24 h-24' src={CustomerService} alt="Customer Service Icon" id='representativeIcon' />
               </div>
-              <h3>Step 2</h3>
-              <p>24/7 access call from our one of our representatives to confirm your enquiry<br></br> with payment processing.</p>
+              <h3 className='underline-offset-2' >Step 2</h3>
+              <p className='ml-2'>24/7 access call from our one of our representatives to confirm your enquiry<br></br> with payment processing.</p>
 
 
             </div>
@@ -60,7 +60,7 @@ function About() {
               <div className='iconWrapper' id='keysWrapper'>
                 <img className='aboutIcon w-24 h-24' src={getKeys} alt='Get keys' id='keygivingIcon' />
               </div>
-              <h3>Step 3</h3>
+              <h3 className='underline-offset-2'>Step 3</h3>
               <p>Pick up your car and zoom away!</p>
             </div>
           </div>
