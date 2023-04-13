269863e0ac6fe650844caf852322aad2b87ed851
chinapicke <chinapickering@hotmail.co.uk>
2023-04-14 00:36:22 +0800

    styling changes to FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,15 +7,19 @@ import { useState } from 'react'
 const FAQquestions = [
     {
         question: 'Why ZoomAway?',
-        answer: 'We are an independent car rental company that started in 2021 and always put our customers first. We offer affordable cars to rent whilst allowing you to choose your vechile, requested destination and time. We highly value our customers experiece and will always try to help problem solve issues if they occur.'
+        answer: 'We are an independent car rental company that started in 2021 and always put our customers first. We offer affordable cars to rent whilst allowing you to choose your vechile, requested destination and time. We highly value our customers experiece and will always try to help problem solve issues if they occur.',
+        id:'questionOne'
     },
     {
         question: 'How long will I have to wait until my enquiry is confirmed?',
-        answer: 'Once you submit your enquiry, we aim to call you within 12 hours. If you enquire outside of our opening hours, one of our representatives will aim to call you by 7am the nexy day.'
+        answer: 'Once you submit your enquiry, we aim to call you within 12 hours. If you enquire outside of our opening hours, one of our representatives will aim to call you by 7am the nexy day.',
+        id:'questionTwo'
+
     },
     {
         question: 'Who is able to rent from us?',
-        answer: 'Anyone can rent from us if you are aged 18 and over, certified driving license for a minimum of 2 years and available funds to rent.'
+        answer: 'Anyone can rent from us if you are aged 18 and over, certified driving license for a minimum of 2 years and available funds to rent.',
+        id:'questionThree'
     }
 ]
 
@@ -47,7 +51,7 @@ function FAQ() {
                                 <span>{selectedFAQ === i ? <FontAwesomeIcon icon={faChevronUp} style={{ color: "#000000", }} /> : <FontAwesomeIcon icon={faChevronDown} style={{ color: "#000000", }} />}</span>
                             </div>
                             <div className={selectedFAQ === i ? 'questionAnswer show' : 'questionAnswer'}>
-                                <h3 className='FAQanswer'>{item.answer}</h3>
+                                <h3 className='FAQanswer' id={item.id}>{item.answer}</h3>
                             </div>
                         </div>
                     )}
