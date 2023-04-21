585a8020c4cb2cdcb7f100f000106a3595a8ddf2
chinapicke <chinapickering@hotmail.co.uk>
2023-04-21 11:47:21 +0800

    responsive vechile page

--- src/Pages/Vechiles.js
+++ src/Pages/Vechiles.js
@@ -13,20 +13,20 @@ function Vechiles() {
         <>
         <HeroBanner />
         <h1 className=' vechilePgTitle text-5xl font-bold my-8 text-center pt-6'>Our Vechiles</h1>
-        <div className='carCards grid grid-cols-2 md:grid-cols-3 gap-4'>
+        <div className='carCards grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-4'>
             {carsAvailable.map((card) =>
                 <div className=" indiviCar max-w-sm m-5 border border-gray-200 rounded-lg shadow flex flex-col justify-between">
                     <a href="#">
                         <img src={card.img} alt="car options" className="px-2" />
                     </a>
                     <div className="p-5 carContent">
-                            <div className='carName mb-2 text-3xl font-bold tracking-tight '>
+                            <div className='carName mb-1 font-bold tracking-tight '>
                                 <h2 >{card.make} {card.model}</h2>
                             </div>
-                            <div className='carPrice mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                                <h2 className='font-semibold text-center'>£ {card.pricePd}</h2>
+                            <div className='carPrice mb-1  font-bold tracking-tight text-gray-900'>
+                                <h2 className='font-semibold '>£ {card.pricePd}</h2>
                             </div>
-                        <div className='carInfo grid grid-cols-2'>
+                        <div className='carInfo grid grid-cols-2 gap-x-8'>
                             <div className='carFuel mb-2 text-md font-bold tracking-tight text-gray-900'>
                                 <p>Fuel Type: {card.fuel}</p>
                             </div>
@@ -41,7 +41,7 @@ function Vechiles() {
                             </div>
                         </div>
                         <Link to="/">
-                        <button className='bookBtn text-white text-center font-bold rounded-lg px-5 py-2.5 mr-2 mb-2 mt-4 flex  ' >
+                        <button className='bookBtn text-white text-center font-bold rounded-lg px-5 py-2.5 mb-2 mt-4 ' >
                             Book Now</button>
 
                         </Link>
